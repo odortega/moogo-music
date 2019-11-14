cce9a3f68ac6332ef2accf40b2bf6722012261b8
Oscar Ortega <odortega@gmail.com>
2019-11-13 23:49:11 -0500

    Update register service on AuthenticationService

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -8,20 +8,20 @@ export class AuthenticateService {
 
   constructor(private storage: Storage) { }
 
-  async loginUser(credential){
-    let user = await this.storage.get('user');
-    return new Promise((accept, reject)=>{
-      if(credential.email == user.email && btoa(credential.password)== user.password){
-      //if(credential.email == 'test@test.com' && credential.password=='12345'){
+  async loginUser(credential) {
+    const user = await this.storage.get('user');
+    return new Promise((accept, reject) => {
+      if (user.email == credential.email &&
+        user.password == btoa(credential.password)) {
         accept("Login correcto");
-      }else{
+      } else {
         reject("Login incorrecto");
       }
     });
   }
 
-  registerUser(userData){
-   userData.password =  btoa(userData.password);
-    return  this.storage.set('user', userData);
+  registerUser(userData) {
+    userData.password = btoa(userData.password);
+    return this.storage.set('user', userData);
   }
 }
