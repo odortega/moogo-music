b47f1b4855b938d6b510d8266f2d662dacb83f6d
Oscar Ortega <odortega@gmail.com>
2019-11-17 01:37:51 -0500

    Add platzi music service and add slides to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlatziMusicService } from '../services/platzi-music.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  artists = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
+  songs: any[]=[];
+  albums: any[]=[];
+  slideOps = {
+    initialSlide: 2,
+    slidesPerView: 4,
+    centeredSlides: true,
+    speed: 400
+  };
+  constructor(private musicService: PlatziMusicService) { }
 
-  constructor() { }
-
+  ionViewDidEnter() {
+    this.musicService.getNewReleases().then(newReleases => {
+      this.artists = newReleases.albums.items;
+      this.songs = newReleases.albums.items.filter(
+        e => e.album_type == "single");
+      this.albums = newReleases.albums.items.filter(
+        e => e.album_type == "album");
+    });
+  }
 }
