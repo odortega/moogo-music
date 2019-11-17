588fdc9c7c09607c02796b7980f4643cd9cfafef
Oscar Ortega <odortega@gmail.com>
2019-11-17 00:32:20 -0500

    completed menu page

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuController, NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController,
+    private navCtrl: NavController,
+    private storage: Storage
+  ) { }
 
   ngOnInit() {
   }
 
+  closeMenu() {
+    this.menu.close();
+  }
+
+  logout() {
+    this.storage.set('isUserLoggedIn',false);
+    this.navCtrl.navigateRoot('/login');
+  }
 }
