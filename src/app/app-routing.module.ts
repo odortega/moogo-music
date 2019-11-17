30ee859e5165789c7f12234b83fc3a44823aeb9d
Oscar Ortega <odortega@gmail.com>
2019-11-17 00:02:36 -0500

    Add menu page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule),
-    canActivate: [LoginGuard, IntroGuard]
-  },
   { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  {
+    path: 'menu', loadChildren: './menu/menu.module#MenuPageModule',
+    canActivate: [LoginGuard, IntroGuard]
+  },
 ];
 
 @NgModule({
